dbe861d7258cf9bf628b1201d7a13e26b3c18d3d
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2023-02-06 09:45:39 +0100

    add one about ENV

---
+++ Slurm/with_ENV.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --partition=E5
+#SBATCH --cpus-per-task=1           # one CPU
+#SBATCH --mem=2G                    # 2GiB mem overall
+#SBATCH --time=0-00:10:00           # day-hours:minutes:seconds
+#SBATCH --export=ALL                # export all $ENV variables to compute node
+
+env > env-"${$SLURMD_NODENAME}".txt
+
