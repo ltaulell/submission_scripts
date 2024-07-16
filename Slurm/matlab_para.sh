6d6c51d61e215c66d51297ad4d19e4998ee5ceb4
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2024-07-16 15:51:57 +0200

    proper matlab examples, no "exclusive"

--- Slurm/matlab_para.sh
+++ Slurm/matlab_para.sh
@@ -2,15 +2,11 @@
 #SBATCH --job-name=test_matlab
 #SBATCH --partition=E5
 #SBATCH --cpus-per-task=16          # -n
+#SBATCH --mem=63G                   # global to node, in GiB
 #SBATCH --ntasks=1
-#SBATCH --time=0-01:00:00           # day-hours:minutes:seconds
-#SBATCH --nodes=1                   # -N
-#SBATCH --exclusive                 # exclusive mode, ie: this node will be mine
-
+#SBATCH --time=0-03:00:00           # day-hours:minutes:seconds
 
 module use /applis/PSMN/debian11/Generic/modules/all/
 module load Matlab/R2019b
 
 matlab -nodisplay -nodesktop -nojvm -nosplash script.m
-
-
---
+++ Slurm/matlab_seq.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=test_matlab
+#SBATCH --partition=E5-short
+#SBATCH --cpus-per-task=1           # -n
+#SBATCH --mem-per-cpu=500           # in MiB
+#SBATCH --ntasks=1
+#SBATCH --time=0-0:30:00            # day-hours:minutes:seconds
+
+module use /applis/PSMN/debian11/Generic/modules/all/
+module load Matlab/R2019b
+
+matlab -nodisplay -nodesktop -nojvm -nosplash script.m
