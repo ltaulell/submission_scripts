178de7990d0e48a7155889f4420afabc19959c1f
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2025-01-09 11:33:24 +0100

    proper binding & practices with GPU

--- Slurm/with_2GPU_1x2.sh
+++ Slurm/with_2GPU_1x2.sh
@@ -2,10 +2,11 @@
 #SBATCH --job-name=test
 #SBATCH --partition=E5-GPU
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=2           # or more
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-gpu=1
-#SBATCH --mem-per-cpu=12G
+#SBATCH --gpu-bind=verbose,single:1 # bind each process to its own GPU (single:<tasks_per_gpu>)
+#SBATCH --mem-per-cpu=12G           # good practice to match GPU mem
 #SBATCH --time=0-00:10:00           # day-hours:minutes:seconds
 #
 #SBATCH -o ./%N.%j.%x.out           # output
@@ -15,11 +16,13 @@
 # %x = jobname
 # %j = jobid
 
-env
+#env  # for heavy debug purpose
 echo "--- *** --- *** ---"
-nvidia-smi
+nvidia-smi -L  # usable device
 
 # usable GPU devices will be listed (comma separated list) in both
 # CUDA_VISIBLE_DEVICES and GPU_DEVICE_ORDINAL, if you need their id (0 or 1)
 
-
+echo "cuda visible devices: ${CUDA_VISIBLE_DEVICES}"
+echo "gpu device ordinal: ${GPU_DEVICE_ORDINAL}"
+echo "slurm allocated gpu: ${SLURM_JOB_GPUS}"
--- Slurm/with_2GPU_2x1.sh
+++ Slurm/with_2GPU_2x1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=test
 #SBATCH --partition=E5-GPU
-#SBATCH --ntasks=1
+#SBATCH --ntasks=2
 #SBATCH --cpus-per-task=1           # or more
-#SBATCH --gres=gpu:1
-#SBATCH --gpu-bind=verbose,single:1
+#SBATCH --gres=gpu:2
 #SBATCH --cpus-per-gpu=1
+#SBATCH --gpu-bind=verbose,single:1 # bind each process to its own GPU (single:<tasks_per_gpu>)
 #SBATCH --gpus-per-task=1           # equivalent of --gpu-bind=per_task,single
 #SBATCH --mem-per-cpu=12G           # good practice to match GPU mem
 #SBATCH --time=0-00:10:00           # day-hours:minutes:seconds
