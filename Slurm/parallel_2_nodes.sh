4ee78ea921d17322c651b002ff538fdaa0bbb7ab
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2025-02-03 14:34:27 +0100

    add some explanations (user request)

--- Slurm/parallel_2_nodes.sh
+++ Slurm/parallel_2_nodes.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=test
 #SBATCH --partition=E5
-#SBATCH --nodes=2
-#SBATCH --exclusive
-#SBATCH --ntasks-per-node=16
-#SBATCH --cpus-per-task=1
 #SBATCH --time=0-00:10:00           # day-hours:minutes:seconds
+#SBATCH --exclusive
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
 
 cd $SLURM_SUBMIT_DIR
-mpirun -np $SLURM_NTASKS mybin < input > output
+mpirun -np $SLURM_NPROCS mybin < input > output
 
+# Cores repartition depends on binary cores handling, and can be very tricky.
+# You can have (ntasks-per-node x cpus-per-task)= $SLURM_NPROCS from different
+# combinations: 2n x 16t x 1c = 32c OR 2n x 1t x 16c = 32c
+# Best is to do some tests :)
